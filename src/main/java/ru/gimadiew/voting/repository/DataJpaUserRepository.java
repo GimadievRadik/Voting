79a082c0878f38e0ca9f43b2bb6f8fac5f6db901
Gimadiev Radik <gimadiew@gmail.com>
2020-04-08 17:31:06 +0300

    AdminRestController ready

--- src/main/java/ru/gimadiew/voting/repository/DataJpaUserRepository.java
+++ src/main/java/ru/gimadiew/voting/repository/DataJpaUserRepository.java
@@ -1,10 +1,21 @@
 package ru.gimadiew.voting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.gimadiew.voting.model.User;
 
 @Repository
+@Transactional(readOnly = true)
 public interface DataJpaUserRepository extends JpaRepository<User, Integer> {
 
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.id=:id")
+    int delete(@Param("id") int id);
+
+    User getByEmail(String email);
 }
--- src/main/java/ru/gimadiew/voting/web/user/AbstractUserController.java
+++ src/main/java/ru/gimadiew/voting/web/user/AbstractUserController.java
@@ -3,14 +3,14 @@ package ru.gimadiew.voting.web.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 import ru.gimadiew.voting.model.User;
 import ru.gimadiew.voting.repository.DataJpaUserRepository;
 import ru.gimadiew.voting.repository.UserRepository;
 
 import java.util.List;
 
-import static ru.gimadiew.voting.util.ValidationUtil.assureIdConsistent;
-import static ru.gimadiew.voting.util.ValidationUtil.checkNew;
+import static ru.gimadiew.voting.util.ValidationUtil.*;
 
 public abstract class AbstractUserController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
@@ -22,31 +22,34 @@ public abstract class AbstractUserController {
         log.info("getAll");
         return userRepository.findAll();
     }
-//
-//    public User get(int id) {
-//        log.info("get {}", id);
-//        return userRepository.get(id);
-//    }
-//
-//    public User create(User user) {
-//        log.info("create {}", user);
-//        checkNew(user);
-//        return userRepository.save(user);
-//    }
-//
-//    public void delete(int id) {
-//        log.info("delete {}", id);
-//        userRepository.delete(id);
-//    }
-//
-//    public void update(User user, int id) {
-//        log.info("update {} with id={}", user, id);
-//        assureIdConsistent(user, id);
-//        userRepository.update(user);
-//    }
-//
-//    public User getByMail(String email) {
-//        log.info("getByEmail {}", email);
-//        return userRepository.getByEmail(email);
-//    }
+
+    public User get(int id) {
+        log.info("get {}", id);
+        return checkNotFoundWithId(userRepository.findById(id).orElse(null), id);
+    }
+
+    public User create(User user) {
+        log.info("create {}", user);
+        checkNew(user);
+        Assert.notNull(user, "user must not be null");
+        return userRepository.save(user);
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        checkNotFoundWithId(userRepository.delete(id) != 0, id);
+    }
+
+    public void update(User user, int id) {
+        log.info("update {} with id={}", user, id);
+        assureIdConsistent(user, id);
+        Assert.notNull(user, "user must not be null");
+        checkNotFoundWithId(userRepository.save(user), user.id());
+    }
+
+    public User getByMail(String email) {
+        log.info("getByEmail {}", email);
+        Assert.notNull(email, "email must not be null");
+        return checkNotFound(userRepository.getByEmail(email), "email=" + email);
+    }
 }
